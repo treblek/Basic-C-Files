a92526b133b5ed72a56990817edb10e356ffdc8e
Tayfun Blacktea <45109186+treblek@users.noreply.github.com>
2019-03-05 20:16:54 +0200

    Create Found and Delete in a String.C

---
+++ Found and Delete in a String.C
@@ -0,0 +1,58 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdint.h"
+
+uint8_t karakterSayisi(char*);
+void bulVeSil(char*,char*);
+int main(void)
+{
+	
+	printf("Lutfen cumle giriniz.");
+	char *karakterDizisi,*silinecekKarakterDizisi;
+	gets(karakterDizisi);
+	printf("Lutfen silinecek kelimeyi giriniz. ");
+	gets(silinecekKarakterDizisi);
+	system("cls");
+	printf("Eski Hali :%s\n",karakterDizisi);
+	bulVeSil(karakterDizisi,silinecekKarakterDizisi);
+	printf("Yeni Hali %s",karakterDizisi);
+		return -1;
+}
+void bulVeSil(char*karakterDizisi,char*silinecekKarakterDizisi)
+{
+	uint8_t donguDegiskeni,ikinciDonguDegiskeni,yardimciDegisken=0;
+	for(donguDegiskeni=0;donguDegiskeni<karakterSayisi(karakterDizisi)-karakterSayisi(silinecekKarakterDizisi);donguDegiskeni++)
+	{
+		for(ikinciDonguDegiskeni=0;ikinciDonguDegiskeni<karakterSayisi(silinecekKarakterDizisi);ikinciDonguDegiskeni++)
+		{
+			if(*(karakterDizisi+donguDegiskeni+ikinciDonguDegiskeni) ==*(silinecekKarakterDizisi+ikinciDonguDegiskeni))
+			{
+				yardimciDegisken++;
+			}
+		}
+		if(yardimciDegisken== karakterSayisi(silinecekKarakterDizisi))
+		{
+				for(ikinciDonguDegiskeni=0;ikinciDonguDegiskeni<karakterSayisi(silinecekKarakterDizisi);ikinciDonguDegiskeni++)
+		{
+			*(karakterDizisi+donguDegiskeni+ikinciDonguDegiskeni) =*(silinecekKarakterDizisi+ikinciDonguDegiskeni);
+		}
+		break;
+		}
+		yardimciDegisken =0;
+	}
+	for(ikinciDonguDegiskeni = donguDegiskeni;ikinciDonguDegiskeni<=karakterSayisi(karakterDizisi)-karakterSayisi(silinecekKarakterDizisi);ikinciDonguDegiskeni++)
+	{
+		*(karakterDizisi+ikinciDonguDegiskeni) = *(karakterDizisi+ikinciDonguDegiskeni+karakterSayisi(silinecekKarakterDizisi));
+		*(karakterDizisi+ikinciDonguDegiskeni+karakterSayisi(silinecekKarakterDizisi)) = '0';
+	}
+}
+uint8_t karakterSayisi(char*karakterDizisi)
+{
+	uint8_t donguDegiskeni=0;
+	while(*(karakterDizisi+donguDegiskeni) != '\0')
+	{
+		donguDegiskeni++;
+	}
+	return donguDegiskeni;
+}
